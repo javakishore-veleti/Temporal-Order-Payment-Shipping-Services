6da90042b66c6bb087454817aebc7109805bb757
javakishore-veleti <javakishore@gmail.com>
2025-09-27 10:18:20 -0400

    Initial Commit

---
+++ src/main/java/com/jk/poc/spring_boot/temporal_poc/TemporalOrderPaymentShippingServicesApplication.java
@@ -0,0 +1,13 @@
+package com.jk.poc.spring_boot.temporal_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemporalOrderPaymentShippingServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemporalOrderPaymentShippingServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jk/poc/spring_boot/temporal_poc/TemporalOrderPaymentShippingServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.poc.spring_boot.temporal_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemporalOrderPaymentShippingServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
